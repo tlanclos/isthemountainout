9390f4271ba7edfdd826f9e24a724a7a42e7f658
Taylor <tlanclos@live.com>
2020-09-04 11:27:23 -0700

    Create an app that downloads mountain images

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import request from "request";
+import fs from "fs";
+import cron from "node-cron";
+import express from "express";
+import moment from "moment";
+
+const app = express();
+
+cron.schedule("0 0,30 * * * *", () => {
+  const uri = "http://backend.roundshot.com/cams/241/original";
+  downloadOne(uri, { outputDir: "TrainingData/Unclassified" });
+});
+
+app.listen(3128);
+
+function downloadOne(uri: string, options: Options) {
+  const now = moment();
+  const filename = `MountRainier_${now.format("YYYY-MM-DDTHHmmss")}.jpg`;
+  const fullFilename = `${options.outputDir}/${filename}`;
+
+  fs.mkdir(options.outputDir, { recursive: true }, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      request(uri)
+        .pipe(fs.createWriteStream(fullFilename))
+        .on("close", () => {
+          console.log(`Saved ${fullFilename}`);
+        });
+    }
+  });
+}
+
+interface Options {
+  outputDir: string;
+}
