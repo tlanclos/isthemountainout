2c01230860bcc24da9a3ad85429c70bbf5ac1092
Taylor <tlanclos@live.com>
2020-09-06 21:44:08 -0700

    Attempt branding and "fake" posting of images

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import requests
 import shutil
 import os
+from PIL import Image
+from google.cloud import storage
+from typing import Optional
 from trainer.classifier import Classifier, ClassifierOptions
+from trainer.common.path import get_script_path
 
 classifier = None
+branding = None
+STATE_FILE = '/is-the-mountain-out-state.txt'
 
 
 def classify(request):
@@ -20,7 +26,17 @@ def classify(request):
 
     classification, confidence, image = classifier.classify(__download_image(
         "http://backend.roundshot.com/cams/241/original"))
-    return f'{classification} {confidence:.2f}%'
+    branded_image = __brand_image(image)
+
+    data = request.get_json(force=True)
+    state = __get_last_classification(data['bucket'])
+    if state != classification:
+        posted_image = True
+        __update_last_classification(data['bucket'])
+    else:
+        posted_image = False
+
+    return f'{classification} {confidence:.2f}% {posted_image}'
 
 
 def __download_image(url: str, *, to_file: str = '/tmp/current.jpg') -> str:
@@ -34,3 +50,31 @@ def __download_image(url: str, *, to_file: str = '/tmp/current.jpg') -> str:
         return to_file
     else:
         raise IOError('Could not download latest image')
+
+
+def __brand_image(image: Image.Image) -> Image.Image:
+    global branding
+    if branding is None:
+        branding = Image.open(os.path.join(
+            get_script_path(), '..', 'branding', 'branding_1920x1080.png'))
+
+    image = image.crop((7036, 162, 8956, 1242))
+    image.paste(branding, (0, 0), branding)
+    return image
+
+
+def __get_last_classification(bucket: str) -> Optional[str]:
+    client = storage.Client()
+    bucket = client.get_bucket(bucket)
+    blob = bucket.get_blob(STATE_FILE)
+    if not blob:
+        return None
+    else:
+        return blob.download_as_string().decode('utf-8')
+
+
+def __update_last_classification(bucket: str, *, state: str) -> None:
+    client = storage.Client()
+    bucket = client.get_bucket(bucket)
+    blob = bucket.get_blob(STATE_FILE)
+    blob.update_from_string(state)
