37d3de14a5e2de8ef90be7bc46a430732dbaf06b
Taylor <tlanclos@live.com>
2020-10-01 23:24:09 -0700

    Update mountain model and weights

--- trainer/v2/main.py
+++ trainer/v2/main.py
@@ -102,43 +102,47 @@ def __gen_model(classes: int) -> tf.keras.Model:
     shape = (224, 224, 3)
     inputs = tf.keras.Input(shape=shape)
     outputs = model.chained(
-        tf.keras.layers.add([
-            model.chained(
-                inputs,
-                tf.keras.layers.Lambda(tf.image.rgb_to_grayscale),
-                tf.keras.layers.Conv2D(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Conv2D(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Conv2D(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Flatten(),
-                tf.keras.layers.Dropout(0.2),
-                tf.keras.layers.Dense(32),
-            ),
-            model.chained(
-                inputs,
-                tf.keras.layers.Lambda(tf.image.rgb_to_grayscale),
-                tf.keras.layers.Conv2DTranspose(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Conv2DTranspose(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Conv2DTranspose(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Flatten(),
-                tf.keras.layers.Dropout(0.2),
-                tf.keras.layers.Dense(32),
-            ),
-        ]),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.Dense(classes),
-        tf.keras.layers.Activation('softmax'))
+    tf.keras.layers.add([
+        model.chained(
+            inputs,
+            tf.keras.layers.Lambda(
+                lambda image: tf.image.rgb_to_grayscale(image)),
+            tf.keras.layers.Conv2D(
+                filters=64, kernel_size=3, activation='relu'),
+            tf.keras.layers.MaxPooling2D(2),
+            tf.keras.layers.Conv2D(
+                filters=64, kernel_size=3, activation='relu'),
+            tf.keras.layers.MaxPooling2D(2),
+            tf.keras.layers.Dropout(0.1),
+            tf.keras.layers.Conv2D(
+                filters=64, kernel_size=3, activation='relu'),
+            tf.keras.layers.MaxPooling2D(2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(32),
+        ),
+        model.chained(
+            inputs,
+            tf.keras.layers.Lambda(
+                lambda image: tf.image.rgb_to_grayscale(image)),
+            tf.keras.layers.Conv2DTranspose(
+                filters=64, kernel_size=3, activation='relu'),
+            tf.keras.layers.MaxPooling2D(2),
+            tf.keras.layers.Conv2DTranspose(
+                filters=64, kernel_size=3, activation='relu'),
+            tf.keras.layers.MaxPooling2D(2),
+            tf.keras.layers.Dropout(0.1),
+            tf.keras.layers.Conv2DTranspose(
+                filters=64, kernel_size=3, activation='relu'),
+            tf.keras.layers.MaxPooling2D(2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(32),
+        ),
+    ]),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(len(classes)),
+    tf.keras.layers.Activation('softmax'))
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     model.build(input_shape=(None, *shape))
     model.load_weights('isthemountainout.h5')
