bc0b35229c0bf35b3af473ae9e251cc9ae5514ad
Taylor <tlanclos@live.com>
2020-11-13 16:49:53 -0800

    Use Xception model trained with mountain data

--- trainer/v2/common/frozenmodel.py
+++ trainer/v2/common/frozenmodel.py
@@ -14,47 +14,70 @@ def generate() -> tf.keras.Model:
     shape = (224, 224, 3)
     inputs = tf.keras.Input(shape=shape)
     outputs = m.chained(
-        tf.keras.layers.add([
+        # Entry Flow
+        tf.keras.layers.Conv2D(filters=32, kernel_size=3, strides=2, padding='same'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Activation('relu'),
+        m.expand(
+            flow=lambda previous_activation, size: tf.keras.layers.add([
+                m.chained(
+                    m.duplicate(
+                        layers=lambda: [
+                            tf.keras.layers.Activation('relu'),
+                            tf.keras.layers.SeparableConv2D(filters=size, kernel_size=3, padding='same'),
+                            tf.keras.layers.BatchNormalization(),
+                        ],
+                        count=2
+                    ),
+                    tf.keras.layers.MaxPooling2D(pool_size=3, strides=2, padding='same'),
+                )(previous_activation),
+                tf.keras.layers.Conv2D(filters=size, kernel_size=1, strides=2, padding='same')(previous_activation),
+            ]),
+            values=[128, 256, 728],
+        ),
+
+        # # Middle Flow
+        m.expand(
+            flow=lambda previous_activation, _: tf.keras.layers.add([
+                m.duplicate(
+                    layers=lambda: [
+                        tf.keras.layers.Activation('relu'),
+                        tf.keras.layers.SeparableConv2D(filters=728, kernel_size=3, padding='same'),
+                        tf.keras.layers.BatchNormalization(),
+                    ],
+                    count=3,
+                )(previous_activation),
+                previous_activation,
+            ]),
+            values=[0] * 8
+        ),
+
+        # # Exit Flow
+        lambda previous_activation: tf.keras.layers.add([
             m.chained(
-                inputs,
-                tf.keras.layers.Lambda(
-                    lambda image: tf.image.rgb_to_grayscale(image)),
-                tf.keras.layers.Conv2D(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Conv2D(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Dropout(0.1),
-                tf.keras.layers.Conv2D(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Flatten(),
-                tf.keras.layers.Dropout(0.2),
-                tf.keras.layers.Dense(32),
-            ),
-            m.chained(
-                inputs,
-                tf.keras.layers.Lambda(
-                    lambda image: tf.image.rgb_to_grayscale(image)),
-                tf.keras.layers.Conv2DTranspose(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Conv2DTranspose(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Dropout(0.1),
-                tf.keras.layers.Conv2DTranspose(
-                    filters=64, kernel_size=3, activation='relu'),
-                tf.keras.layers.MaxPooling2D(2),
-                tf.keras.layers.Flatten(),
-                tf.keras.layers.Dropout(0.2),
-                tf.keras.layers.Dense(32),
-            ),
+                tf.keras.layers.Activation('relu'),
+                tf.keras.layers.SeparableConv2D(filters=728, kernel_size=3, padding='same'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Activation('relu'),
+                tf.keras.layers.SeparableConv2D(filters=1024, kernel_size=3, padding='same'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.MaxPooling2D(pool_size=3, strides=2, padding='same'),
+            )(previous_activation),
+            tf.keras.layers.Conv2D(filters=1024, kernel_size=1, strides=2, padding='same')(previous_activation),
         ]),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.Dense(len(labels())),
-        tf.keras.layers.Activation('softmax'))
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.SeparableConv2D(filters=728, kernel_size=3, padding='same'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.SeparableConv2D(filters=1024, kernel_size=3, padding='same'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(len(labels()), activation='linear'),
+        tf.keras.layers.Dropout(0.2),
+    )(inputs)
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     model.build(input_shape=(None, *shape))
     model.load_weights('isthemountainout.h5')
--- trainer/v2/common/model.py
+++ trainer/v2/common/model.py
@@ -10,12 +10,27 @@ from functools import reduce
 
 
 def chained(*layers):
-    assert len(layers) > 1, 'chainned layers must have at least 2 layers'
-    return reduce(lambda acc, curr: curr(acc), layers)
+    assert len(layers) >= 1, 'chainned layers must have at least 1 layer'
+    def __chained(input):
+        return reduce(lambda acc, curr: curr(acc), layers, input)
+    return __chained
 
 
 def merge(*layers, merger):
     return merger(list(layers))
+
+
+def duplicate(*, layers, count):
+    return chained(*[layer for layer in itertools.chain(*[layers() for _ in range(count)])])
+
+
+def expand(*, flow, values):
+    def __expand(input):
+        previous_activation = input
+        for value in values:
+            previous_activation = flow(previous_activation, value)
+        return previous_activation
+    return __expand
 
 
 class LogConfusionMatrixCallback(tf.keras.callbacks.Callback):
