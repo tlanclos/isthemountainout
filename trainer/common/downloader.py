565c24b0ec383f2aef4d9f43833d750db3ca70d0
Taylor <tlanclos@live.com>
2021-07-21 20:48:08 -0700

    Check image date prior to testing classification

--- trainer/common/downloader.py
+++ trainer/common/downloader.py
@@ -1,16 +1,21 @@
 import requests
 import shutil
 import io
+from urllib.parse import urlparse
+from datetime import datetime, date as Date
 from PIL import Image
+from typing import Tuple
 
-def download_image(url: str) -> Image:
+def download_image(url: str) -> Tuple[Image.Image, Date]:
     redirected_url = requests.head(url, allow_redirects=True).url
+    # Example format: https://storage.roundshot.com/544a1a9d451563.40343637/2021-07-02/14-40-00/2021-07-02-14-40-00_original.jpg
+    date = datetime.fromisoformat(list(filter(None, urlparse(redirected_url).path.split('/')))[1])
     req = requests.get(redirected_url, stream=True)
     if req.status_code == 200:
         req.raw.decode_content = True
         data = io.BytesIO()
         shutil.copyfileobj(req.raw, data)
         data.seek(0)
-        return Image.open(data)
+        return Image.open(data), date
     else:
         raise IOError(f'Could not download latest image from {url} -> {redirected_url}', req)
