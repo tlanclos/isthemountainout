cf458a84fe081a49ef6e57958ebb7190303a3340
Taylor <tlanclos@live.com>
2022-10-23 01:22:25 -0700

    Setup infrastructure for training model.
    Still need to get confusion matrix properly set up for testing the model's accuracy

--- v2/common/dataset.py
+++ v2/common/dataset.py
@@ -1,21 +1,32 @@
+import time
 import tensorflow as tf
+from typing import Tuple
 
 
 class Dataset:
-    original: tf.data.Dataset
+    directory: str
+    batch_size: int
+    image_size: Tuple[int, int]
     training: tf.data.Dataset
     validation: tf.data.Dataset
-    test: tf.data.Dataset
+    seed: int
 
-    def __init__(self, dataset: tf.data.Dataset, *, splits=(0.8, 0.1, 0.1)):
-        (training_split, validation_split, test_split) = splits
-        assert (training_split + validation_split + test_split) == 1
+    def __init__(self, directory: str, *, batch_size=32, image_size=(1920, 1080)):
+        self.seed = int(time.time())
+        self.directory = directory
+        self.batch_size = batch_size
+        self.image_size = image_size
 
-        dataset_size = tf.data.experimental.cardinality(dataset).numpy()
-        training_size = int(dataset_size * training_split)
-        validation_size = int(dataset_size * validation_split)
+        self.training = self.dataset_for('training')
+        self.validation = self.dataset_for('validation')
 
-        self.original = dataset
-        self.training = dataset.take(training_size)
-        self.validation = dataset.skip(training_size).take(validation_size)
-        self.test = dataset.skip(training_size).skip(validation_size)
+    def dataset_for(self, subset: str):
+        return tf.keras.utils.image_dataset_from_directory(
+            self.directory,
+            label_mode='categorical',
+            seed=self.seed,
+            validation_split=0.2,
+            subset=subset,
+            batch_size=self.batch_size,
+            image_size=self.image_size,
+        )
