030317ad4ea3146fb98b2584129a55d0b6ee06c5
Taylor <tlanclos@live.com>
2022-12-11 17:10:40 -0800

    Reorganize trainer a bit

--- v2/common/frozenmodel.py
+++ v2/common/frozenmodel.py
@@ -1,14 +1,15 @@
+import os
 import tensorflow as tf
 import common.model as m
 
 from tensorflow.keras.layers import RandomTranslation, RandomBrightness, Cropping2D, Dense, Dropout, GlobalAveragePooling2D, Conv2D, BatchNormalization, MaxPooling2D, SeparableConv2D, Activation, add
-from typing import List
+from typing import List, Optional
 from enum import Enum
 
 
 class Label(Enum):
     NIGHT = 'Night'
-    NOT_VISIBLE = 'NotVisible'
+    HIDDEN = 'Hidden'
     MYSTICAL = 'Mystical'
     BEAUTIFUL = 'Beautiful'
 
@@ -17,25 +18,28 @@ def labels() -> List[Label]:
     return sorted(Label.__members__.values(), key=lambda label: label.value)
 
 
-def generate_model(*, weights_filepath: str):
+def generate_model(*, weights_filepath: Optional[str], with_augmentations: bool = False, can_ignore_weights: bool = False):
     shape = (1080, 1920, 3)
     inputs = tf.keras.Input(shape=shape)
+
+    translation_amount = 0.12 if with_augmentations else 0.0
+    brightness_amount = 0.10 if with_augmentations else 0.0
     outputs = m.chained(
         # Augmentations
         RandomTranslation(
-            0.0,
-            0.0,
+            translation_amount,
+            translation_amount,
             fill_mode='nearest'
         ),
-        RandomBrightness(0.0),
+        RandomBrightness(brightness_amount),
         # Cropping2D parameter are how much to take off of top, bottom,
         # left and right, not a rectangle of the cropped image
         Cropping2D(cropping=((160, 350), (580, 450))),
         # Entry Flow
-        Conv2D(filters=32, kernel_size=3, strides=2, padding='same'),
+        Conv2D(filters=12, kernel_size=5, strides=2, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-        Conv2D(filters=64, kernel_size=3, padding='same'),
+        Conv2D(filters=12, kernel_size=5, padding='same'),
         BatchNormalization(),
         Activation('relu'),
         m.expand(
@@ -56,7 +60,7 @@ def generate_model(*, weights_filepath: str):
                 Conv2D(filters=size, kernel_size=1, strides=2,
                        padding='same')(previous_activation),
             ]),
-            values=[128, 256, 728],
+            values=[32, 16, 8],
         ),
 
         # Middle Flow
@@ -66,7 +70,7 @@ def generate_model(*, weights_filepath: str):
                     layers=lambda: [
                         Activation('relu'),
                         SeparableConv2D(
-                            filters=728, kernel_size=3, padding='same'),
+                            filters=8, kernel_size=3, padding='same'),
                         BatchNormalization(),
                     ],
                     count=3,
@@ -104,5 +108,8 @@ def generate_model(*, weights_filepath: str):
     )(inputs)
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     model.build(input_shape=(None, *shape))
-    model.load_weights(weights_filepath)
+    if weights_filepath and os.path.exists(weights_filepath):
+        model.load_weights(weights_filepath)
+    elif not can_ignore_weights:
+        raise Exception(f'No weights found for {weights_filepath}')
     return model
