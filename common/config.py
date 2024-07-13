686569d939e225190f6193471d8a4b3dcaa7b24c
Taylor Lanclos <tlanclos@live.com>
2024-07-12 20:01:28 -0700

    Create classify and brand configuration schema

--- common/config.py
+++ common/config.py
@@ -1,3 +1,10 @@
+import yaml
+import marshmallow
+import marshmallow.fields
+
+from marshmallow_oneofschema import OneOfSchema
+
+
 def mountain_history_bucket_name() -> str:
     return 'mountain-history'
 
@@ -36,3 +43,56 @@ def twitter_api_key_bucket_name():
 
 def twitter_api_key_filename():
     return 'v2/twitter-keys.json'
+
+
+class FileConfigurationSchema(marshmallow.Schema):
+    path = marshmallow.fields.String(required=True)
+
+
+class DirectoryConfigurationSchema(marshmallow.Schema):
+    path = marshmallow.fields.String(required=True)
+
+
+class BrandConfigurationSchema(marshmallow.Schema):
+    file = marshmallow.fields.Nested(FileConfigurationSchema, required=True)
+
+
+class LiveImageProviderConfigurationSchema(marshmallow.Schema):
+    pass
+
+
+class SnapshotImageProviderConfigurationSchema(marshmallow.Schema):
+    directory = marshmallow.fields.Nested(
+        DirectoryConfigurationSchema, required=True)
+
+
+class ImageProviderConfigurationSchema(OneOfSchema):
+    type_schemas = {
+        'live': LiveImageProviderConfigurationSchema,
+        'snapshot': SnapshotImageProviderConfigurationSchema,
+    }
+
+
+class DatabaseProviderConfigurationSchema(OneOfSchema):
+    type_schemas = {'sqlite': FileConfigurationSchema}
+
+
+class ClassifyConfigurationSchema(marshmallow.Schema):
+    image_provider = marshmallow.fields.Nested(
+        ImageProviderConfigurationSchema, required=True)
+    tracker = marshmallow.fields.Nested(
+        DatabaseProviderConfigurationSchema, required=True)
+    weights = marshmallow.fields.Nested(FileConfigurationSchema, required=True)
+
+
+class RootConfigurationSchema(marshmallow.Schema):
+    brand = marshmallow.fields.Nested(BrandConfigurationSchema, required=True)
+    classify = marshmallow.fields.Nested(
+        ClassifyConfigurationSchema, required=True)
+
+
+with open('.test-config/configuration.yaml', 'r') as f:
+    data = yaml.safe_load(f)
+
+val = RootConfigurationSchema().load(data)
+print(val)
