812cb91711469e51ab10ec01d431981e86efdb81
Taylor Lanclos <tlanclos@live.com>
2024-07-14 12:07:57 -0700

    Update dry run code so that it writes, but doesn't post

--- classify.py
+++ classify.py
@@ -10,35 +10,37 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 parser = argparse.ArgumentParser(
     description='Classify an image of Mount Rainier')
 parser.add_argument(
-    '--dry-run',
+    '--enable-posting',
     action='store_true',
-    help='Classify the image and print to the console, but nothing is committed')
+    help='Classify the image, print to the console, and store the classification result, but nothing is posted to the internet')
 parser.add_argument(
     '--config',
     type=create_root_config,
     help='Path to the configuration file')
 
 
-def main(*, config: RootConfiguration, dry_run: bool = False):
+def main(*, config: RootConfiguration, post: bool = False):
     classifier, tracker = config.classifier, config.classification_tracker
     classification, image = classifier.classify_next()
     print('Classification', classification)
 
-    if tracker.should_post(classification.classification):
-        classification.was_posted = True
+    classification.should_post = tracker.should_post(
+        classification.classification)
+    if classification.should_post:
         print(f'Posting {classification}')
-        if not dry_run:
-            tracker.amend(classification)
+        if not post:
+            classification.was_posted = True
             print('Branding image')
             branded_image = ImageEditor(image).brand(brand=config.brand.get())
             for publisher in config.publishers:
                 publisher.post(branded_image, classification=classification)
+
     else:
         print(f'Classification did not change from {classification}')
-        if not dry_run:
-            tracker.amend(classification)
+
+    tracker.amend(classification)
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(config=args.config, dry_run=args.dry_run)
+    main(config=args.config, post=args.enable_posting)
